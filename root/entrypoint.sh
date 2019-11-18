d272d006b5c26f38c45eae0053f5332357951bbc
Stefan Ciubotaru <stefanelul2000@users.noreply.github.com>
2019-11-18 11:47:26 +0000

    Updated Github link

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -6,7 +6,7 @@ if [[ ! -d /app/.git ]]; then
   echo '----------------------'
   echo '| Cloning PythonChat |'
   echo '----------------------'
-  git clone https://stefanelul2000:cc5aefcbc3c8b1818d21bf141622a565be9e5116@github.com/stefanelul2000/pythonchat.git /app
+  git clone https://github.com/stefanelul2000/pythonchat.git /app
   cd /app || (echo 'Failed to load PythonChat repository folder'; exit)
 elif [[ -d /app/.git ]]; then
   echo '----------------------'
